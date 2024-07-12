e96f3c5ec1ed664f65f3f042fef986a014d0d4be
gobear6212 <85070296+gobear6212@users.noreply.github.com>
2024-07-12 22:23:52 +0800

    upload

--- js/utils-0.1.0.js
+++ js/utils-0.1.0.js
@@ -22,9 +22,32 @@ function delay(millisecond) {   // in ms
     return new Promise(resolve => setTimeout(resolve, millisecond));
 }
 
+// https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+function cyrb53(str, seed = 0) {
+    let h1 = 0xdeadbeef ^ seed;
+    let h2 = 0x41c6ce57 ^ seed;
+
+    for(let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+
+    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
 async function hashString(string, algorithm="SHA-1") {
-    let bits = await crypto.subtle.digest(algorithm, new TextEncoder().encode(string));
-    return Array.from(new Uint8Array(bits)).join('');
+    if (crypto.subtle) {
+        const bits = await crypto.subtle.digest(algorithm, new TextEncoder().encode(string));
+        return Array.from(new Uint8Array(bits)).join('');
+    } else {
+        cyrb53(string);
+    }
 }
 
 function stateEventListener(elem, event, stateChecker, callback) {
